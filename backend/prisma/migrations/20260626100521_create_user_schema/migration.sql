dc07b1e019fac61609713cebb88cb1c6f798b1ba
kumar <kumarsumit10042005@gmail.com>
2026-06-26 15:50:37 +0530

    create prisma client

---
+++ backend/prisma/migrations/20260626100521_create_user_schema/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CUSTOMER', 'RESTAURANT_OWNER', 'SHELTER_MANAGER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" INTEGER NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'CUSTOMER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+import {PrismaPg} from '@prisma/adapter-pg'
+import {PrismaClient} from '@prisma/client'
+
+const connectionSting = process.env.DATABASE_URL
+
+const adapter = new PrismaPg ({connectionString})
+
+const prisma = new PrismaClient({
+     adapter,
+     log:[query]
+});
+
+export {prisma};
